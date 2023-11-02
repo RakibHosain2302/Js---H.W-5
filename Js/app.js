7900b2e90e05ad8a6944fc897b48fc384736936a
Rakib Hosain <143322856+RakibHosain2302@users.noreply.github.com>
2023-11-02 21:07:58 +0600

    Add files via upload

---
+++ Js/app.js
@@ -0,0 +1,37 @@
+
+let counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    let endingValue = counter.dataset.value;
+    let count = 0;
+    let duration = 1000
+
+
+    let counting = setInterval(() =>{
+          counter.innerHTML = count++; 
+         if(count > endingValue){
+             clearInterval(counting)
+         }
+
+        
+   
+    }, (duration / endingValue));
+
+  
+    
+});
+
+
+// successfully msg
+
+let btn = document.querySelector('.btn');
+let msg = document.querySelector('.msg');
+
+btn.addEventListener('click',() => {
+    msg.classList.add("active")
+
+
+    setTimeout(() => {
+        msg.classList.remove("active")
+    }, 5000);
+} )
